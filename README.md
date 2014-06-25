bff2d7399dbb98d81a5f6cff7ba39f9204d3dec2
piyuranjan <piyuranjan@gmail.com>
2014-06-25 19:12:27 -0400

    added function for importing fastq files from a path

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ NucleaseIndelActivity
 =====================
 
 Automation for the quantification of indels arising due to nuclease activity
+
+##Desired components for future:
+* Import Fastq files using a config file
--- indelQuantificationFromFastqPaired.pl
+++ indelQuantificationFromFastqPaired.pl
@@ -19,6 +19,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Time::HiRes qw(gettimeofday tv_interval);
+use Sort::Naturally;
 
 ##All function definitions
 #sub GetLoggingTime;
@@ -50,6 +51,8 @@ Usage:\n$0 [options: provide all flags separately]
 \nPackage requirements (in PATH):\nPrinseq-Lite(prinseq-lite.pl); TrimGalore(trim_galore); FLASH(flash); Burrows-Wheeler Aligner(bwa); Samtools(samtools)
 \nOptions:
  -d|dataDir	provide directory path with paired end read files
+			This option assumes paired end files to be ordered
+			next to each other.
 			Default: pwd
  -w|workDir	provide path to create all intermediate/result files
 			Default: pwd/IndelAnalysis
@@ -71,11 +74,39 @@ if(!GetOptions('d|dataDir=s' => \$dataDir,
 				||(!defined $dataDir))
 	{Usage; exit 1;}
 
+my %pairedFqFiles=%{ScanSeqFiles($dataDir)};
+foreach my $key(nsort keys %pairedFqFiles)
+	{print "$key\t$pairedFqFiles{$key}\n";}
+##Write later: Input method by a config file
 
-########## Functions Below ##########
-sub GetLoggingTime
+#####################################
+######### Subroutines Below #########
+#####################################
+
+sub GetLoggingTime #find and return current logging timestamp
 	{
 	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)=localtime(time);
 	my $niceTimestamp = sprintf ( "%04d%02d%02d %02d:%02d:%02d",$year+1900,$mon+1,$mday,$hour,$min,$sec);
 	return $niceTimestamp;
+	}
+
+sub ScanSeqFiles #Scan and guess paired end files from a given location
+	{
+	my $dataDir=$_[0]; #record path for data files
+	my %pairedFqFiles; #will record key->value as pair1File->pair2File
+	opendir(DIR,$dataDir) or die $!;
+	my @seqFiles=nsort(readdir(DIR)); #sorted aphanumerically
+	my @fqFiles;
+	foreach my $seqFile(@seqFiles)
+		{
+		next unless(-f "$dataDir/$seqFile"); #skip any directory/hidden files
+		next unless($seqFile=~/\.f(ast)?q$/i); #skip files not with extension fastq/fq
+		#print $seqFile."\n";
+		push(@fqFiles,$seqFile);
+		}
+	for(my $i=0;$i<=$#fqFiles;$i+=2)
+		{
+		$pairedFqFiles{$fqFiles[$i]}=$fqFiles[$i+1];
+		}
+	return \%pairedFqFiles;
 	}
